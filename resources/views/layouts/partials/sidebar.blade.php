59c577d4cbe2bb6b39db80b9d3068eee8ea9b1b3
Ssmunna <ss.munna2025@gmail.com>
2025-12-02 15:27:59 +0600

    [Add] pagination add and design change

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -38,44 +38,10 @@
     <nav class="p-3 flex-1 space-y-1 text-gray-700">
 
         <!-- Dashboard -->
-        <a href="#" class="flex items-center space-x-3 px-3 py-2 rounded-lg hover:bg-gray-100">
-            <svg class="w-6 h-6 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6
-                             0h6" />
-            </svg>
-
+        <a href="{{route('dashboard')}}" class="flex items-center space-x-3 px-3 py-2 rounded-lg hover:bg-gray-100">
+            <i class="fa-regular fa-house text-xl"></i>
             <span x-show="!sidebarCollapse" x-transition class="font-medium">Dashboard</span>
         </a>
-
-        <!-- Users with Dropdown -->
-        <div x-data="{ open:false }">
-
-            <button @click="open = !open"
-                class="flex items-center w-full space-x-3 px-3 py-2 rounded-lg hover:bg-gray-100">
-
-                <svg class="w-6 h-6 text-indigo-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a4 4 0 00-3-3.87M9 20H4v-2a4 4 0 013-3.87m0 0a4
-                                 4 0 118 0m-8 0v1m8-1v1" />
-                </svg>
-
-                <span x-show="!sidebarCollapse" x-transition class="flex-1 text-left font-medium">
-                    Users
-                </span>
-
-                <svg x-show="!sidebarCollapse" :class="open ? 'rotate-90' : ''" class="w-4 h-4 transition-transform"
-                    fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-width="2" stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
-                </svg>
-
-            </button>
-
-            <!-- Dropdown Items -->
-            <div x-show="open && !sidebarCollapse" x-collapse class="pl-12 space-y-1">
-                <a href="#" class="block px-3 py-2 rounded hover:bg-gray-100">All Users</a>
-                <a href="#" class="block px-3 py-2 rounded hover:bg-gray-100">Add User</a>
-            </div>
-        </div>
-        <!-- Users with Dropdown -->
 
         <!-- Notification Nav -->
         <a href="{{route('admin.notification.list')}}" class="flex items-center space-x-2.5 px-3 py-2 rounded-lg hover:bg-gray-100 {{ Route::is('admin.notification.list') ? 'bg-gray-100 font-semibold' : '' }}">
